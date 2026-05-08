ea9a7fc20ccbd23f8a254d95c2b11a40d4b2508b
StefanJ20 <jojostef76@gmail.com>
2026-05-08 11:41:47 -0700

    ignore changes

--- backend/firebase/urls.py
+++
@@ -1,8 +0,0 @@
-from . import signup, login, profile # type: ignore
-from django.urls import path
-
-urlpatterns = [
-    path("signup/", signup.signup_view, name="signup"),
-    path("login/", login.login_view, name="login"),
-    path("profile/", profile.profile_view, name="profile"),
-]
